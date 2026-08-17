3998dfc7a745075241bd4e9d3c571f79f7ddec52
Aditya Soran <adityasoran@gmail.com>
2026-08-18 00:14:11 +0530

    Done

--- backend/main.py
+++ backend/main.py
@@ -48,7 +48,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://mavenir-rag-chatbot.vercel.app/",
+        "https://mavenir-rag-chatbot.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
